1bdf64634842497d8d250bbd7cd7d229312ea58f
alfred <alfred.rezk@gmail.com>
2025-05-06 20:37:51 -0400

    adding updating and deleting task controllers

--- controllers/tasks.ctrl.js
+++ controllers/tasks.ctrl.js
@@ -7,7 +7,6 @@ const ErrorResponse = require('../utils/ErrorResponse')
 //  @ACCESS Public
 // ----------------------------------------
 exports.getTasks = (req, res) => {
-  console.log(data[0]?.id.length)
   res.status(200).json({
     success: true,
     data,
@@ -20,7 +19,21 @@ exports.getTasks = (req, res) => {
 //  @DESC   Get a single task by id
 //  @ACCESS Public
 // ----------------------------------------
-exports.getSingleTask = (req, res) => {}
+exports.getSingleTask = async (req, res) => {
+  const taskId = req.params.taskId
+
+  // validate the id
+  if (taskId.length !== 36) throw new ErrorResponse(400, 'Invalid task id')
+
+  const task = data.find((task) => task.id === taskId)
+  if (!task) throw new ErrorResponse(404, `Task not found with id ${taskId}`)
+
+  // Sending a success response
+  res.status(200).json({
+    success: true,
+    data: task,
+  })
+}
 
 // ----------------------------------------
 //  @URL    POST  /api/tasks/
@@ -59,11 +72,55 @@ exports.addTask = async (req, res) => {
 //  @DESC   update a single task by id
 //  @ACCESS Private
 // ----------------------------------------
-exports.updateTask = (req, res) => {}
+exports.updateTask = (req, res) => {
+  const taskId = req.params.taskId
+  const { title, description, isCompleted, priority } = req.body
+
+  // validate the id
+  if (taskId.length !== 36) throw new ErrorResponse(400, 'Invalid task id')
+
+  // Find task index in data array
+  const taskIndex = data.findIndex((task) => task.id === taskId)
+  if (taskIndex === -1)
+    throw new ErrorResponse(404, `Task not found with id ${taskId}`)
+
+  //update the task data
+  data[taskIndex].title = title ?? data[taskIndex].title
+  data[taskIndex].description = description ?? data[taskIndex].description
+  data[taskIndex].isCompleted = isCompleted ?? data[taskIndex].isCompleted
+  data[taskIndex].priority = priority ?? data[taskIndex].priority
+
+  // Sending a success response
+  res.status(200).json({
+    success: true,
+    data: data[taskIndex],
+    message: 'Task updated successfully',
+  })
+}
 
 // ----------------------------------------
 //  @URL    DELETE  /api/tasks/:taskId
 //  @DESC   delete a single task by id
 //  @ACCESS Private
 // ----------------------------------------
-exports.deleteTask = (req, res) => {}
+exports.deleteTask = async (req, res) => {
+  const taskId = req.params.taskId
+
+  // validate the id
+  if (taskId.length !== 36) throw new ErrorResponse(400, 'Invalid task id')
+
+  // Find task index in data array
+  const taskIndex = data.findIndex((task) => task.id === taskId)
+  if (taskIndex === -1)
+    throw new ErrorResponse(404, `Task not found with id ${taskId}`)
+
+  // Delete task from data array
+  data.splice(taskIndex, 1)
+  // Sending a success response
+  res.status(200).json({
+    success: true,
+    data: {},
+    count: data.length,
+    message: 'Task deleted successfully',
+  })
+}
